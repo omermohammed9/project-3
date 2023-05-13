0b53ae73e4f362d6de2587875be2d479d38daf66
Omar Mohammed <om17102@auis.edu.krd>
2023-05-13 22:11:48 +0300

    A few Tweaks to App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,77 +2,50 @@
 <script setup>
 import ChatList from "@/components/ChatList.vue";
 import {ref} from "vue";
+
 const conversations = ref([]);
-const addConversation = (title, messages) => {
+
+const addConversation = (title, messages = []) => {
   conversations.value.push({ title, messages });
 };
 
-addConversation("Chat with Abdulazeez", [
-  {
-    outgoing: true,
-    text: "Hello, World!",
-    username: "hdrm147"
-  },
-  {
-    outgoing: false,
-    text: "Hello, Haidar!",
-    username: "azeez"
-  }
-]);
+const startNewConversation = (username) => {
+  addConversation(`Chat with ${username}`);
+};
 
-addConversation("Chat with Ahmed", [
-  {
-    outgoing: true,
-    text: "Loooop",
-    username: "hdrm147"
-  },
-  {
-    outgoing: false,
-    text: "Hello, Haidar!",
-    username: "azeez"
+const addMessageToConversation = (conversationIndex, message) => {
+  const conversation = conversations.value[conversationIndex];
+  
+  if(conversation) {
+    conversation.messages.push(message);
+  } else {
+    console.error(`No conversation found at index ${conversationIndex}`);
   }
-]);
+};
 
-addConversation("Chat with Ahmed", [
-  {
-    outgoing: true,
-    text: "Loooop",
-    username: "hdrm147"
-  },
-  {
-    outgoing: false,
-    text: "Hello, Haidar!",
-    username: "azeez"
-  }
-]);
-addConversation("Chat with Ali", [
-  {
-    outgoing: true,
-    text: "Loooop",
-    username: "hdrm147"
-  },
-  {
-    outgoing: false,
-    text: "Hello, Haidar!",
-    username: "azeez"
-  }
-]);
-addConversation("Chat with Mohammed", [
-  {
-    outgoing: true,
-    text: "Loooop",
-    username: "hdrm147"
-  },
-  {
-    outgoing: false,
-    text: "Hello, Haidar!",
-    username: "azeez"
-  }
-]);
+// Add initial conversations
+startNewConversation("Abdulazeez");
+addMessageToConversation(0, {
+  outgoing: true,
+  text: "Hello, World!",
+  username: "hdrm147",
+});
 
+addMessageToConversation(0, {
+  outgoing: false,
+  text: "Hello, Abdulazeez!",
+  username: "azeez",
+});
 
+startNewConversation("Ahmed");
+addMessageToConversation(1, {
+  outgoing: true,
+  text: "Hi Ahmed!",
+  username: "hdrm147",
+});
 
 </script>
+
 
 
 <template>
