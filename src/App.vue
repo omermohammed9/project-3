d0e7da13f9e88a82a2557d861555b8748d637ef3
Omar Mohammed <om17102@auis.edu.krd>
2023-05-14 00:12:21 +0300

    New Changes to APP.vue

--- src/App.vue
+++ src/App.vue
@@ -9,38 +9,10 @@ const addConversation = (title, messages = [], username) => {
 };
 
 
-const startNewConversation = () => {
-  fetch('https://randomuser.me/api/?format=json')
-    .then(response => response.json())
-    .then(data => {
-      // Extract the user's name from the response
-      const user = data.results[0];
-      const username = user.login.username;
-      
-      // Add the new conversation with the random user's name as the title
-      addConversation(`Chat with ${username}`, [], username);
-    })
-    .catch(error => {
-      console.error('Error fetching user:', error);
-    });
+const startNewConversation = (username) => {
+  addConversation(`Chat with ${username}`, [], username);
 };
 
-fetch('https://randomuser.me/api/')
-  .then(response => response.json())
-  .then(data => {
-    // Extract the user information from the response
-    const user = data.results[0];
-    
-    // Access the user's name and other details
-    const firstName = user.name.first;
-    const lastName = user.name.last;
-    const username = user.login.username;
-    
-    // You can use the extracted user details here if needed
-  })
-  .catch(error => {
-    console.error('Error fetching user:', error);
-  });
 const addMessageToConversation = (conversationIndex, message) => {
   const conversation = conversations.value[conversationIndex];
 
@@ -52,13 +24,13 @@ const addMessageToConversation = (conversationIndex, message) => {
 };
 
 // Add the new function here
-const handleNewMessage = (conversationIndex, text) => {
+const handleNewMessage = (conversationIndex, text, fromUserName) => {
   const conversation = conversations.value[conversationIndex];
   if (conversation) {
     addMessageToConversation(conversationIndex, {
       outgoing: true,
       text: text,
-      username: conversation.username, // Use the username from the conversation
+      username: fromUserName, // Use the username from the conversation
     });
   } else {
     console.error(`No conversation found at index ${conversationIndex}`);
@@ -66,34 +38,54 @@ const handleNewMessage = (conversationIndex, text) => {
 };
 
 // Add initial conversations
-startNewConversation("Abdulazeez");
-addMessageToConversation(0, {
-  outgoing: true,
-  text: "Hello, World!",
-  username: "hdrm147",
-});
 
-addMessageToConversation(0, {
-  outgoing: false,
-  text: "Hello, Abdulazeez!",
-  username: "azeez",
-});
+
+startNewConversation("Abdulazeez");
+handleNewMessage(0, "Hello, World!", "hdrm147");
+handleNewMessage(0, "Hello, Abdulazeez!", "azeez");
 
 startNewConversation("Ahmed");
-
-addMessageToConversation(1, {
-  outgoing: true,
-  text: "Hi Ahmed!",
-  username: "hdrm147",
-});
+handleNewMessage(1, "Hi Ahmed!", "hdrm147");
 
 startNewConversation("Omer");
+handleNewMessage(2, "Hi Omer!", "e7pm");
+handleNewMessage(2, "Hi Omer!", "e7pm");
+// startNewConversation("Abdulazeez");
+// addMessageToConversation(0, {
+//   outgoing: true,
+//   text: "Hello, World!",
+//   username: "hdrm147",
+// });
 
-addMessageToConversation(2, {
-  outgoing: true,
-  text: "Hi Omer!",
-  username: "e7pm",
-});
+
+// addMessageToConversation(0, {
+//   outgoing: false,
+//   text: "Hello, Abdulazeez!",
+//   username: "azeez",
+// });
+
+
+
+// startNewConversation("Ahmed");
+
+// addMessageToConversation(1, {
+//   outgoing: true,
+//   text: "Hi Ahmed!",
+//   username: "hdrm147",
+// });
+
+// startNewConversation("Omer");
+
+// addMessageToConversation(2, {
+//   outgoing: true,
+//   text: "Hi Omer!",
+//   username: "e7pm",
+// });
+// addMessageToConversation(2, {
+//   outgoing: true,
+//   text: "Hi Omer!",
+//   username: "e7pm",
+// });
 
 // ... and so on for other conversations
 </script>
